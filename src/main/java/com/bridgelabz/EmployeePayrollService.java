37301c9a6f5902e2bf140c9d669114e9a28a1b19
KumarB95 <kumarbalingal@gmail.com>
2021-10-29 22:41:06 +0530

    Refactored : Store Employee Payroll in file

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -13,6 +13,12 @@ import java.util.Scanner;
 public class EmployeePayrollService {
     public List<EmployeePayrollData> employeePayrollList;
 
+    public enum IOService {CONSOLE_IO, FILE_IO}
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
     public EmployeePayrollService() {
         employeePayrollList = new ArrayList<>();
     }
@@ -22,7 +28,7 @@ public class EmployeePayrollService {
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.printWelcomeMessage();
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
+        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
     }
 
     /* This method is implementing Welcome Message */
@@ -32,6 +38,7 @@ public class EmployeePayrollService {
 
     /**
      * Purpose : To read the information of Employees from the console
+     *
      * @param consoleInputReader takes the information of employees
      */
     private void readEmployeePayrollData(Scanner consoleInputReader) {
@@ -46,7 +53,18 @@ public class EmployeePayrollService {
     }
 
     /* This method is implementing to write the Employee Payroll to the console */
-    private void writeEmployeePayrollData() {
-        System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrollList);
+    public void writeEmployeePayrollData(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO)) {
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
+        }
+    }
+
+    /* This method is implementing to count entries in a file */
+    public long countEntries(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            return new EmployeePayrollFileIOService().countEntries();
+        return 0;
     }
 }
